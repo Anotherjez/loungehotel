13335641ec6c504fcc94bd9eee2d968c80769cbf
Jesus Mercado <anotherjez2@gmail.com>
2020-10-23 21:27:36 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,155 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>LoungeHotel</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="js/jquery-1.6.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>
+<script type="text/javascript" src="js/Adamina_400.font.js"></script>
+<script type="text/javascript" src="js/jquery.jqtransform.js" ></script>
+<script type="text/javascript" src="js/script.js" ></script>
+<script type="text/javascript" src="js/kwicks-1.5.1.pack.js" ></script>
+<script type="text/javascript" src="js/atooltip.jquery.js"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<link rel="stylesheet" href="css/ie.css" type="text/css" media="all">
+<![endif]-->
+</head>
+<body id="page1">
+<div class="bg1">
+  <div class="bg2">
+    <div class="main">
+      <!-- header -->
+      <header>
+        <h1><a href="index.html" id="logo">LoungeHotel</a></h1>
+        <div class="department"> 9863 - 9867 Mill Road, LA, MG09 99HT<br>
+          <span>Freephone: &nbsp; +1 800 559 6580</span> </div>
+      </header>
+      <div class="box">
+        <nav>
+          <ul id="menu">
+            <li class="active"><a href="inicio.html">Nosotros</a></li>
+            <li><a href="servicios.html">Servicios</a></li>
+            <li><a href="reserva.html">Reserva</a></li>
+            <li><a href="rooms.html">Rooms</a></li>
+            <li><a href="locacion.html">Locacion</a></li>
+          </ul>
+        </nav>
+        <!-- header end -->
+        <!-- content -->
+        <article id="content">
+          <div class="box1">
+            <div class="wrapper">
+              <form action="#" id="form1">
+                <h2>Busca una habitacion</h2>
+                <fieldset>
+                  <div class="row">
+                    <input type="text" class="input">
+                    Nombre: </div>
+                  <div class="row">
+                    <input type="text" class="input">
+                    E-Mail: </div>
+                  <div class="row">
+                    <input type="text" class="input">
+                    Telefono: </div>
+                  <div class="row">
+                    <div class="select1">
+                      <select>
+                        <option>&nbsp;</option>
+                        <option>...</option>
+                      </select>
+                    </div>
+                    
+                    Fecha de llegada: </div>
+                  <div class="row_textarea"> Comentario Adicional:
+                    <textarea cols="1" rows="1"></textarea>
+                  </div>
+                  <div class="wrapper"> <a href="#" class="button1">Enviar</a> <a href="#" class="button1">Limpiar</a> </div>
+                </fieldset>
+              </form>
+              <div class="kwicks-wrapper marg_bot1">
+                <ul class="kwicks horizontal">
+                  <li><img src="images/img1.jpg" alt=""></li>
+                  <li><img src="images/img2.jpg" alt=""></li>
+                  <li><img src="images/img3.jpg" alt=""></li>
+                  <li><img src="images/img4.jpg" alt=""></li>
+                </ul>
+              </div>
+            </div>
+            <div class="pad">
+              <div class="line1">
+                <div class="wrapper line2">
+                  <div class="col1">
+                    <h2><img src="images/title_marker1.jpg" alt="">Mejores Tarifas</h2>
+                    <p class="pad_bot1">Lounge Hotel es un hotel lujoso y versátil que, gracias a su estratégica ubicación en Santo Domingo y servicios como Wi-Fi gratuito, es ideal para viajes en familia, en pareja o para visitas y reuniones de negocios. </p>
+                    <a href="#" class="color1">Mas..</a> </div>
+                  <div class="col1 pad_left1">
+                    <h2><img src="images/title_marker2.jpg" alt="">Hotel Guide</h2>
+                    <p class="pad_bot1">La capital de la República Dominicana es una ciudad estimulante y multicultural y, por eso, LoungueHotel es un punto de partida ideal para conocer la zona histórica y colonial; y empaparse de toda la idiosincrasia dominicana</p>
+                    <a href="#" class="color1">Mas..</a> </div>
+                  <div class="col1 pad_left1">
+                    <h2><img src="images/title_marker3.jpg" alt="">Amenidades</h2>
+                    <p class="pad_bot1">Disfute de esta espectacular piscina de borde infinito ubicada en el tercer nivel del hotel, junto al BLU Bar, el SPA y el gimnasio. ¡Deleitese con los atardeceres mas hermosos y sumerjase en el mejor area para relajarse del hotel!</p>
+                    <a href="#" class="color1">Mas..</a> </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="pad">
+            <div class="wrapper line3">
+              <div class="col2">
+                <h2>Bienvenido a nuestro Hotel!</h2>
+                <p class="pad_bot1"><strong class="color2">Ubicado en el centro de entretenimiento y negocios de la ciudad y cerca de los mejores centros corporativos, centros comerciales, restaurantes y bares. La Zona Colonial de Santo Domingo está a solo ocho km del hotel. </p>
+                <p class="pad_bot2"> Este nuevo hotel ofrece innovadora gastronomía con tres restaurantes de concepto único y dos bares. Relájese en el Spa o disfrute de la piscina de borde infinito ubicada en el 3er piso. Todas las habitaciones están equipadas con ventanas anti-ruido, internet de alta velocidad y más.</p>
+                <a href="#" class="button1">Read More</a> </div>
+              
+          </div>
+        </article>
+        <!--content end-->
+      </div>
+    </div>
+  </div>
+</div>
+<div class="main">
+  <!-- footer -->
+  <footer>
+    <div class="col2">Copyright &copy; <a href="#">Laura Ulerio</a> Todos los derechos reservados  <a target="_blank" href="http://www.templatemonster.com/"></a>
+      <nav>
+        <ul id="footer_menu">
+          <li class="active"><a href="index.html">Nosotros</a></li>
+            <li><a href="services.html">Servicios</a></li>
+            <li><a href="booking.html">Reserva</a></li>
+            <li><a href="rooms.html">Rooms</a></li>
+            <li><a href="locations.html">Locacion</a></li>
+
+        </ul>
+      </nav>
+    </div>
+    <div class="col1 pad_left1">
+      <ul id="icons">
+        <li><a href="#" class="normaltip"><img src="images/icon1.jpg" alt=""></a></li>
+        <li><a href="#" class="normaltip"><img src="images/icon2.jpg" alt=""></a></li>
+        <li><a href="#" class="normaltip"><img src="images/icon3.jpg" alt=""></a></li>
+        <li><a href="#" class="normaltip"><img src="images/icon4.jpg" alt=""></a></li>
+      </ul>
+    </div>
+    <!-- {%FOOTER_LINK} -->
+  </footer>
+  <!-- footer end -->
+</div>
+<script type="text/javascript">Cufon.now();</script>
+<script type="text/javascript">
+$(document).ready(function () {
+    $('.kwicks').kwicks({
+        max: 500,
+        spacing: 0,
+        event: 'mouseover'
+    });
+})
+</script>
+</body>
+</html>
---
+++ reserva.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>LoungeHotel | Booking</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="js/jquery-1.6.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>
+<script type="text/javascript" src="js/Adamina_400.font.js"></script>
+<script type="text/javascript" src="js/jquery.jqtransform.js" ></script>
+<script type="text/javascript" src="js/script.js" ></script>
+<script type="text/javascript" src="js/atooltip.jquery.js"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<link rel="stylesheet" href="css/ie.css" type="text/css" media="all">
+<![endif]-->
+</head>
+<body id="page3">
+<div class="bg1">
+  <div class="bg2">
+    <div class="main">
+      <!-- header -->
+      <header>
+        <h1><a href="index.html" id="logo">LoungeHotel</a></h1>
+        <div class="department"> 9863 - 9867 Mill Road, LA, MG09 99HT<br>
+          <span>Freephone: &nbsp; +1 800 559 6580</span> </div>
+      </header>
+      <div class="box">
+        <nav>
+          <ul id="menu">
+            <li class="active"><a href="index.html">Nosotros</a></li>
+            <li><a href="services.html">Servicios</a></li>
+            <li><a href="booking.html">Reserva</a></li>
+            <li><a href="rooms.html">Rooms</a></li>
+            <li><a href="locations.html">Locacion</a></li>
+          </ul>
+        </nav>
+        <!-- header end -->
+        <!-- content -->
+        <article id="content">
+          <div class="box1">
+            <div class="wrapper">
+              <form action="#" id="form1">
+                <h2>Busca una habitacion</h2>
+                <fieldset>
+                  <div class="row">
+                    <input type="text" class="input">
+                    Nombre: </div>
+                  <div class="row">
+                    <input type="text" class="input">
+                    E-Mail: </div>
+                  <div class="row">
+                    <input type="text" class="input">
+                    Telefono: </div>
+                  <div class="row">
+                    <div class="select1">
+                      <select>
+                        <option>&nbsp;</option>
+                        <option>...</option>
+                      </select>
+                    </div>
+                    
+                    Fecha de llegada: </div>
+                  <div class="row_textarea"> Comentario Adicional:
+                    <textarea cols="1" rows="1"></textarea>
+                  </div>
+                  <div class="wrapper"> <a href="#" class="button1">Enviar</a> <a href="#" class="button1">Limpiar</a> </div>
+                </fieldset>
+              </form>
+              <div class="col2 pad">
+                <h2><img src="images/title_marker1.jpg" alt="">Las mejores propuestas de este mes</h2>
+                <div class="wrapper line1">
+                  <div class="col3">
+                    <figure class="pad_bot3"><img src="images/page3_img1.jpg" alt=""></figure>
+                    <p class="pad_bot1"><strong class="color3">Habitacion Familiar</strong></p>
+                    <p>Hermosa y espaciosa habitacion familiar por solo 200USD p/p/n.</p>
+                    <ul class="list2">
+                      <li><span>2</span>Habitaciones</li>
+                      <li><span>4</span>Camas</li>
+                    </ul>
+                    <a href="#" class="button2">Buscar habitacion</a> </div>
+                  <div class="col3 pad_left2">
+                    <figure class="pad_bot3"><img src="images/page3_img4.jpg" alt=""></figure>
+                    <p class="pad_bot1"><strong class="color3">Lujosa habitacion para pareja</strong></p>
+                    <p>Aprovecha la oferta de esta lujosa habitacion para dos personas: 200USD p/p/n.</p>
+                    <ul class="list2">
+                      <li><span>2</span>Habitacines</li>
+                      <li><span>4</span>Camas</li>
+                    </ul>
+                    <a href="#" class="button2">Buscar habitacion</a> </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="pad">
+            <div class="wrapper line3">
+              <div class="col2">
+                <h2>Sobre la reservacion</h2>
+                <p class="pad_bot1"><strong class="color2">¡EXCLUSIVO EN WEB OFICIAL! HASTA 50% DE DESCUENTO Y 2 NIÑOS GRATIS!</strong> </p>
+                <p>No dejes escapar esta oferta exclusiva y ahorra hasta un 50% en estancias hasta Abril 2021! Además, 2 niños se alojan gratis</p>
+                <div class="wrapper">
+                  <figure class="left marg_right1"><img src="images/page3_img3.jpg" alt=""></figure>
+                  <p class="pad_bot1"><strong class="color2">PRECIO ESPECIAL SOLO ALOJAMIENTO (SERVICIO DE COMIDAS EXCLUIDO)</strong></p>
+                  <p class="pad_bot2"> Reserva ahora tu esperada escapada en tarifa flexible. Nos hemos asegurado de seguir todas las recomendaciones de higiene y salud para que disfrutes de una estancia segura.</p>
+                </div>
+                <div class="pad_bot2">
+                  <div class="wrapper line2">
+                    <div class="col1">
+                      
+                    </div>
+                  </div>
+                </div>
+                <a href="#" class="button1">Leer mas..</a> </div>
+              <div class="col1 pad_left1">
+                <h2>Info</h2>
+                <p class="pad_bot1"><strong class="color2">PRECIO ESPECIAL SOLO ALOJAMIENTO</strong></p>
+                <p class="pad_bot1"> Reserva ahora tu esperada escapada en tarifa flexible. Nos hemos asegurado de seguir todas las recomendaciones de higiene y salud para que disfrutes de una estancia segura.</p>
+               
+                <a href="#" class="button1">Leer mas..</a> </div>
+            </div>
+          </div>
+        </article>
+        <!--content end-->
+      </div>
+    </div>
+  </div>
+</div>
+<div class="main">
+  <!-- footer -->
+  <footer>
+    <div class="col2">Copyright &copy; <a href="#">Rosa Ivett</a> Todos los derechos reservados  <a target="_blank" href="http://www.templatemonster.com/"></a>
+      <nav>
+        <ul id="footer_menu">
+          <li class="active"><a href="index.html">Nosotros</a></li>
+            <li><a href="services.html">Servicios</a></li>
+            <li><a href="booking.html">Reserva</a></li>
+            <li><a href="rooms.html">Rooms</a></li>
+            <li><a href="locations.html">Locacion</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="col1 pad_left1">
+      <ul id="icons">
+        <li><a href="#" class="normaltip"><img src="images/icon1.jpg" alt=""></a></li>
+        <li><a href="#" class="normaltip"><img src="images/icon2.jpg" alt=""></a></li>
+        <li><a href="#" class="normaltip"><img src="images/icon3.jpg" alt=""></a></li>
+        <li><a href="#" class="normaltip"><img src="images/icon4.jpg" alt=""></a></li>
+      </ul>
+    </div>
+    <!-- {%FOOTER_LINK} -->
+  </footer>
+  <!-- footer end -->
+</div>
+<script type="text/javascript">Cufon.now();</script>
+</body>
+</html>
