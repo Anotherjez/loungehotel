4bad19ba459ebaa903bf9473642cf31c64cfb50a
Jesus Mercado <anotherjez2@gmail.com>
2020-10-23 22:22:50 -0400

    connection to db

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-include('../libs/utils.php');
+include('./libs/utils.php');
 
 
 ?>
--- libs/utils.php
+++ libs/utils.php
@@ -1,8 +1,6 @@
 <?php
 
-if(file_exists("../libs/configx.php")){
-  include('../libs/configx.php');
-}
-include('../libs/connection.php');
+include('libs/configx.php');
+include('libs/connection.php');
 
 ?>
